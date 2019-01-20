ad311d9435d7085f563e1a475109111185e02ffc
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-01-20 15:37:24 +0300

    Сделал домашнее задание №2

---
+++ Lesson2_DZ/src/MyArraySizeException.java
@@ -0,0 +1,5 @@
+public class MyArraySizeException extends ArrayIndexOutOfBoundsException{ // Исключение о соответствии массива заданной размерности
+        public MyArraySizeException(String s) {
+        super(s);
+    }
+}
