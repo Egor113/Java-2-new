4f66d96c71d1e4313aa5b0ec894b35026cc02243
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-01-28 20:16:34 +0300

    Deleted unnecessary files

--- Lesson2_DZ/src/MyArraySizeException.java
+++
@@ -1,5 +0,0 @@
-public class MyArraySizeException extends ArrayIndexOutOfBoundsException{ // Исключение о соответствии массива заданной размерности
-        public MyArraySizeException(String s) {
-        super(s);
-    }
-}
