c1b1a0a5f7220f067c5b9741197be38150d1475c
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-02-04 14:19:56 +0300

    Created Server #2

--- Lesson6_DZ/src/server/Server.java
+++ Lesson6_DZ/src/server/Server.java
@@ -1,4 +1,30 @@
 package server;
 
+import com.sun.security.ntlm.Client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public Server(){
+        ServerSocket server = null;
+        Socket socket = null;
+        try {
+            server = new ServerSocket(8090);
+            System.out.println("Server start");
+
+            socket = server.accept();
+            System.out.println("Client connected");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            String line = in.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
