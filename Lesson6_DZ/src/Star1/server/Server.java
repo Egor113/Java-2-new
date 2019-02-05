d4658771c9fce33c3a122e2ce874a2d0b70a7bbd
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-02-05 14:13:50 +0300

    Add ClientHandler #11

--- Lesson6_DZ/src/Star1/server/Server.java
+++ Lesson6_DZ/src/Star1/server/Server.java
@@ -5,23 +5,32 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Server {
     private ServerSocket server;
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<>());
 
     public Server(){
         try {
             server = new ServerSocket(8090);
             System.out.println("Server start");
 
-            socket = server.accept();
-            System.out.println("Client connected");
+//            socket = server.accept();
+//            System.out.println("Client connected");
 
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
+            while (true){
+                socket = server.accept();
+                System.out.println("Client connected");
+                clients.add(new ClientHandler(this,socket));
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -40,6 +49,13 @@ public class Server {
             System.out.println("Message from client: " + in.readUTF());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void broadCast(String msg) {
+        for (ClientHandler handler: clients
+             ) {
+            handler.sendMsg(msg);
         }
     }
 }
